571b3b8040af40cf28cdc2ab97718b0568e3e444
liedsonlm7 <liedsonlm7@gmail.com>
2025-08-19 16:32:06 -0300

    add feature remove item from the cart

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const item2 = await createItem("Mouse RGB", 39.99, 3)
 
 await cartService.addItem(myCart, item1)
 await cartService.addItem(myCart, item2)
+
+await cartService.removeItem(myCart, item2)
+await cartService.removeItem(myCart, item2)
+
 await cartService.displayCart(myCart)
 
 // await cartService.deleteItem(myCart, item1.name)
--- src/services/cart.js
+++ src/services/cart.js
@@ -23,14 +23,33 @@ async function deleteItem(userCart, name) {
 }
 
 // remove item - decrease an item
-async function removeItem(userCart, index) {
+async function removeItem(userCart, item) {
+    // 1. find the item index
+    const indexFound = userCart.findIndex((p) => p.name === item.name)
+    
+    // 2. if you cannot find the item index
+    if (indexFound == -1) {
+        console.log("item não encontrado")
+        return
+    }
 
+    // 3. item > 1 decrease an item
+    if (userCart[indexFound].quantity > 1) {
+        userCart[indexFound].quantity -= 1
+        return
+    } 
+
+    // 4. if item = 1 delete item
+    if (userCart[indexFound].quantity == 1) {
+        userCart.splice(indexFound, 1)
+        return
+    }
 }
 
 async function displayCart(userCart) {
     console.log("\nShopee cart list:")
     userCart.forEach((item, index) => {
-        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal: R$ ${item.subtotal()}`)
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal = R$ ${item.subtotal()}`)
     });
 }
 
